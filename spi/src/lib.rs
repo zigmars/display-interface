8e479be0624ad4240908a1c48d83e54532c2475c
Zigmars Rupenheits <zigmarrs@gmail.com>
2024-03-26 03:40:20 +0200

    rm send_u8 all other data format except [u8]

--- spi/src/lib.rs
+++ spi/src/lib.rs
@@ -18,89 +18,89 @@ where
 {
     match words {
         DataFormat::U8(slice) => spi.write(slice).map_err(|_| DisplayError::BusWriteError),
-        DataFormat::U16(slice) => spi
-            .write(slice.as_byte_slice())
-            .map_err(|_| DisplayError::BusWriteError),
-        DataFormat::U16LE(slice) => {
-            for v in slice.as_mut() {
-                *v = v.to_le();
-            }
-            spi.write(slice.as_byte_slice())
-                .map_err(|_| DisplayError::BusWriteError)
-        }
-        DataFormat::U16BE(slice) => {
-            for v in slice.as_mut() {
-                *v = v.to_be();
-            }
-            spi.write(slice.as_byte_slice())
-                .map_err(|_| DisplayError::BusWriteError)
-        }
-        DataFormat::U8Iter(iter) => {
-            let mut buf = [0; BUFFER_SIZE];
-            let mut i = 0;
-
-            for v in iter.into_iter() {
-                buf[i] = v;
-                i += 1;
-
-                if i == buf.len() {
-                    spi.write(&buf).map_err(|_| DisplayError::BusWriteError)?;
-                    i = 0;
-                }
-            }
-
-            if i > 0 {
-                spi.write(&buf[..i])
-                    .map_err(|_| DisplayError::BusWriteError)?;
-            }
-
-            Ok(())
-        }
-        DataFormat::U16LEIter(iter) => {
-            let mut buf = [0; BUFFER_SIZE];
-            let mut i = 0;
-
-            for v in iter.map(u16::to_le) {
-                buf[i] = v;
-                i += 1;
-
-                if i == buf.len() {
-                    spi.write(buf.as_byte_slice())
-                        .map_err(|_| DisplayError::BusWriteError)?;
-                    i = 0;
-                }
-            }
-
-            if i > 0 {
-                spi.write(buf[..i].as_byte_slice())
-                    .map_err(|_| DisplayError::BusWriteError)?;
-            }
-
-            Ok(())
-        }
-        DataFormat::U16BEIter(iter) => {
-            let mut buf = [0; BUFFER_SIZE];
-            let mut i = 0;
-            let len = buf.len();
-
-            for v in iter.map(u16::to_be) {
-                buf[i] = v;
-                i += 1;
-
-                if i == len {
-                    spi.write(buf.as_byte_slice())
-                        .map_err(|_| DisplayError::BusWriteError)?;
-                    i = 0;
-                }
-            }
-
-            if i > 0 {
-                spi.write(buf[..i].as_byte_slice())
-                    .map_err(|_| DisplayError::BusWriteError)?;
-            }
-
-            Ok(())
-        }
+        // DataFormat::U16(slice) => spi
+        //     .write(slice.as_byte_slice())
+        //     .map_err(|_| DisplayError::BusWriteError),
+        // DataFormat::U16LE(slice) => {
+        //     for v in slice.as_mut() {
+        //         *v = v.to_le();
+        //     }
+        //     spi.write(slice.as_byte_slice())
+        //         .map_err(|_| DisplayError::BusWriteError)
+        // }
+        // DataFormat::U16BE(slice) => {
+        //     for v in slice.as_mut() {
+        //         *v = v.to_be();
+        //     }
+        //     spi.write(slice.as_byte_slice())
+        //         .map_err(|_| DisplayError::BusWriteError)
+        // }
+        // DataFormat::U8Iter(iter) => {
+        //     let mut buf = [0; BUFFER_SIZE];
+        //     let mut i = 0;
+        //
+        //     for v in iter.into_iter() {
+        //         buf[i] = v;
+        //         i += 1;
+        //
+        //         if i == buf.len() {
+        //             spi.write(&buf).map_err(|_| DisplayError::BusWriteError)?;
+        //             i = 0;
+        //         }
+        //     }
+        //
+        //     if i > 0 {
+        //         spi.write(&buf[..i])
+        //             .map_err(|_| DisplayError::BusWriteError)?;
+        //     }
+        //
+        //     Ok(())
+        // }
+        // DataFormat::U16LEIter(iter) => {
+        //     let mut buf = [0; BUFFER_SIZE];
+        //     let mut i = 0;
+        //
+        //     for v in iter.map(u16::to_le) {
+        //         buf[i] = v;
+        //         i += 1;
+        //
+        //         if i == buf.len() {
+        //             spi.write(buf.as_byte_slice())
+        //                 .map_err(|_| DisplayError::BusWriteError)?;
+        //             i = 0;
+        //         }
+        //     }
+        //
+        //     if i > 0 {
+        //         spi.write(buf[..i].as_byte_slice())
+        //             .map_err(|_| DisplayError::BusWriteError)?;
+        //     }
+        //
+        //     Ok(())
+        // }
+        // DataFormat::U16BEIter(iter) => {
+        //     let mut buf = [0; BUFFER_SIZE];
+        //     let mut i = 0;
+        //     let len = buf.len();
+        //
+        //     for v in iter.map(u16::to_be) {
+        //         buf[i] = v;
+        //         i += 1;
+        //
+        //         if i == len {
+        //             spi.write(buf.as_byte_slice())
+        //                 .map_err(|_| DisplayError::BusWriteError)?;
+        //             i = 0;
+        //         }
+        //     }
+        //
+        //     if i > 0 {
+        //         spi.write(buf[..i].as_byte_slice())
+        //             .map_err(|_| DisplayError::BusWriteError)?;
+        //     }
+        //
+        //     Ok(())
+        // }
         _ => Err(DisplayError::DataFormatNotImplemented),
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -39,18 +39,18 @@ pub enum DisplayError {
 pub enum DataFormat<'a> {
     /// Slice of unsigned bytes
     U8(&'a [u8]),
-    /// Slice of unsigned 16bit values with the same endianness as the system, not recommended
-    U16(&'a [u16]),
-    /// Slice of unsigned 16bit values to be sent in big endian byte order
-    U16BE(&'a mut [u16]),
-    /// Slice of unsigned 16bit values to be sent in little endian byte order
-    U16LE(&'a mut [u16]),
-    /// Iterator over unsigned bytes
-    U8Iter(&'a mut dyn Iterator<Item = u8>),
-    /// Iterator over unsigned 16bit values to be sent in big endian byte order
-    U16BEIter(&'a mut dyn Iterator<Item = u16>),
-    /// Iterator over unsigned 16bit values to be sent in little endian byte order
-    U16LEIter(&'a mut dyn Iterator<Item = u16>),
+    // /// Slice of unsigned 16bit values with the same endianness as the system, not recommended
+    // U16(&'a [u16]),
+    // /// Slice of unsigned 16bit values to be sent in big endian byte order
+    // U16BE(&'a mut [u16]),
+    // /// Slice of unsigned 16bit values to be sent in little endian byte order
+    // U16LE(&'a mut [u16]),
+    // /// Iterator over unsigned bytes
+    // U8Iter(&'a mut dyn Iterator<Item = u8>),
+    // /// Iterator over unsigned 16bit values to be sent in big endian byte order
+    // U16BEIter(&'a mut dyn Iterator<Item = u16>),
+    // /// Iterator over unsigned 16bit values to be sent in little endian byte order
+    // U16LEIter(&'a mut dyn Iterator<Item = u16>),
 }
 
 /// This trait implements a write-only interface for a display which has separate data and command
